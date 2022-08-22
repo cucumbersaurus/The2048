c047008aa80856fd4db6cfa168c08dd24f89233c
cucumbersaurus <tallcucumber1234@gmail.com>
2022-08-23 00:17:30 +0900

    모든 방향으로 작동
    대충 만든 코드 ㅋㅋㅋㅋ

--- .vscode/main.cpp
+++ .vscode/main.cpp
@@ -1,21 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <conio.h>
+#include <time.h>
 
 using namespace std;
 
 int board[4][4] = {
-    {2, 2, 0, 0},
-    {2, 2, 2, 2},
-    {4, 2, 2, 4},
-    {0, 2, 2, 4}
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
 };
+
+int last_board[4][4];
 
 void print(){
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            cout<<board[i][j]<<" ";
+            cout<<board[i][j]<<"\t";
         }
-        cout<<endl;
+        cout<<endl<<endl<<endl;
     }
 }
 
@@ -37,7 +40,33 @@ void newNum(){
     }
 }
 
-void left(){
+void rotate(){
+    int tmp = board[0][0];
+    board[0][0] = board[3][0];
+    board[3][0] = board[3][3];
+    board[3][3] = board[0][3];
+    board[0][3] = tmp;
+
+    tmp = board[0][1];
+    board[0][1] = board[2][0];
+    board[2][0] = board[3][2];
+    board[3][2] = board[1][3];
+    board[1][3] = tmp;
+
+    tmp = board[0][2];
+    board[0][2] = board[1][0];
+    board[1][0] = board[3][1];
+    board[3][1] = board[2][3];
+    board[2][3] = tmp;
+
+    tmp = board[1][1];
+    board[1][1] = board[2][1];
+    board[2][1] = board[2][2];
+    board[2][2] = board[1][2];
+    board[1][2] = tmp;
+}
+
+void merge(){
     for(int i=0;i<4;i++){
         for(int j=1;j<4;j++){
             if(board[i][j]!=0){
@@ -85,23 +114,80 @@ void left(){
     }
 }
 
+void copy(){
+    for(int i=0;i<4;i++){
+        for(int j =0;j<4;j++){
+            last_board[i][j] = board[i][j];
+        }
+    }
+}
+
+bool isSame(){
+    for(int i=0;i<4;i++){
+        for(int j =0;j<4;j++){
+            if(last_board[i][j] != board[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void left(){
+    merge();
+}
+
+void right(){
+    rotate();
+    rotate();
+    merge();
+    rotate();
+    rotate();
+}
+
+void up(){
+    rotate();
+    rotate();
+    rotate();
+    merge();
+    rotate();
+}
+
+void down(){
+    rotate();
+    merge();
+    rotate();
+    rotate();
+    rotate();
+}
+
+
 void getKey(){
     int key=_getch();
     cout<<key<<endl;
+
+    copy();
+
     if (key==224){
         key=_getch();
         cout<<key<<endl;
         switch(key){
             case 72://up
+                up();
                 break;
             case 77://right
+                right();
                 break;
             case 80://down
+                down();
                 break;
             case 75://left
                 left();
                 break;
         }
+    }
+    if(!isSame()){
+        newNum();
     }
 }
 
@@ -111,11 +197,13 @@ int main(){
     cin.tie(0);
     srand(time(NULL));
 
+    newNum();
     print();
 
-    getKey();
-
-    print();
+    while(true){
+        getKey();
+        print();
+    }
 
     /*
     for(int i=0;i<30;i++){
@@ -125,7 +213,7 @@ int main(){
         left();
         print();
     }*/
- 
+    
     system("pause");
     return 0;
 }
